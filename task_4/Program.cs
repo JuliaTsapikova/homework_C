d4fc9fe997b5a3fe045a59505405c42b0404e2a0
Julia <tsapikova@mail.ru>
2022-12-16 16:22:05 +0400

    Task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4. На вход подаются 3 числа, вывести максимальное из этих чисел
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+Console.WriteLine($"max = {max}");
