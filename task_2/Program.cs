0d781e7ed52f1e6c54a9972dac130e257e21014a
Julia <tsapikova@mail.ru>
2022-12-16 14:37:43 +0400

    Task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 2. На вход подаются 2 числа, на выходе выдается какое число большее,
+//а какое меньшее
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if(a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
