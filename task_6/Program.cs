194220f28e9979d0b1d57f4c5b13e686302ae13b
Julia <tsapikova@mail.ru>
2022-12-16 16:33:27 +0400

    Task 6

---
+++ task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6. На вход принимается число, на выходе необходимо указать,
+// является ли число четным или нет.
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
